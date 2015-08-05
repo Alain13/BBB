8ceced196bbb6e0caaec571c892066904ef1e9ca
Alain (Debian) <alain.docs@gmail.com>
2015-08-05 20:46:35 +0800

    Adding Hello2 project

---
+++ Hello2/src/Hello.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Hello.cpp
+// Author      : Alain Bartels
+// Version     :
+// Copyright   : Do what you want
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Sup!!!" << endl; // prints !!!Sup!!!
+	return 0;
+}
